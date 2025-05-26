87a6b4a6ba527cad976cd99559353fedbb070771
Nahid Alam Rahat <nahidrahat007@gmail.com>
2025-05-26 16:23:37 +0600

    Implement slider AP

--- lib/app/app_urls.dart
+++ lib/app/app_urls.dart
@@ -1,8 +1,9 @@
 class AppUrls {
 
   static const String _baseUrl = 'https://ecom-rs8e.onrender.com/api';
-
   static const String signUpUrl = '$_baseUrl/auth/signup';
   static const String verifyOtpUrl = '$_baseUrl/auth/verify-otp';
+  static const String signInUrl = '$_baseUrl/auth/login';
+  static const String slidersUrl = '$_baseUrl/slides';
 
 }
